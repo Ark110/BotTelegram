07d7542c0aa9dc02802177cfb32b0e952bcf10fb
adrian.martinez1708 <adrian.martinez1708@gmail.com>
2016-05-28 06:21:13 -0400

    Cambios menores

--- pruebabot.rb
+++ pruebabot.rb
@@ -4,7 +4,7 @@ token = "215010612:AAHAA5cQqubietorU46MG2Tx4pxkT_ECjTM"
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
 		case message.text
-		when "/start" then bot.api.send_message(chat_id: message.chat.id, text: "Bienvenido al bot de pruebad de Adrian \n Los comandos son: \n " + "/start : Mensaje de bienvenida\n /saludo : Te saludo\n /map : Te digo mi Ubicaion \n /stop : Me despido de ti")
+		when "/start" then bot.api.send_message(chat_id: message.chat.id, text: "Bienvenido al bot de prueba de Adrian \n Los comandos son: \n " + "/start : Mensaje de bienvenida\n /saludo : Te saludo\n /map : Te digo mi Ubicaion \n /stop : Me despido de ti")
 		when '/saludo' 
 			bot.api.send_message(chat_id: message.chat.id, text: "Hola #{message.from.first_name}!")
 		when '/stop' 
